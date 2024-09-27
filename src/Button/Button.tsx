2bab692785de38903552c423f7bd2ef14e824e07
blxxp <blxxps@yandex.ru>
2024-09-27 17:31:34 +0300

    add test button style v1

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {ButtonGlass} from "./Button.types";
-
+import style from './button.module.scss';
 export const Button = ({variant, children, className, ...props} : ButtonGlass) => {
     return (
-        <button className={className} {...props}>
+        <button className={style.button} {...props}>
             {children}
         </button>
     );
